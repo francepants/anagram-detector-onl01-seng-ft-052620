e5c0c78648259c308b8dd253689cc8c37cb1ef68
francepants <franceline90@gmail.com>
2020-06-07 18:03:55 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+# should detect no matches
+# should detect a simple anagram
+# should detect an anagram
+# should detect multiple anagrams
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        #instances should respond to a match method that takes an array of possible anagrams. 
+        #It should return all matches in an array. 
+        #If no matches exist, it should return an empty array.
+
+        #iterate over array of the words
+        #compare those words to one in anagram
+        array.select do |something|
+            (@word.split("").sort) == (something.split("").sort)
+        end
+    end
+end
